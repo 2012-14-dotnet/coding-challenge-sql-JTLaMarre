587331ceff904d04c5b10d742c78eaaf3278a9d9
Jacob LaMarre <lordaufchaos@yahoo.com>
2021-01-19 11:07:26 -0500

    report total marketing salary I think

--- scripts/dml.sql
+++ scripts/dml.sql
@@ -6,9 +6,9 @@ FROM Employee.Employee;
 
 insert into Employee.Employee(ID,FirstName,LastName,SSN,DeptID)
 Values
-(1,"Bob","Belcher","123-45-6789",1);
-(2,"Linda","Belcher","123-44-6089",2);
-(3,"Louise","Belcher","423-00-6129",3);
+('1','Bob','Belcher','123-45-6789','1');
+('2','Linda','Belcher','123-44-6089','2');
+('3','Louise','Belcher','423-00-6129','3');
 
 
 SELECT * 
@@ -16,15 +16,39 @@ FROM Employee.EmpDetails;
 
 insert into Employee.EmpDetails(ID,EmployeeID,Salary,Address1,Address2,City,[State],Country)
 Values
-(1,1,"40000","123 Warf ave","Unit B","San Fransico","CA","United States");
-(2,2,"36000","123 Warf ave","Unit B","San Fransico","CA","United States");
-(3,3,"80000","77 Warf St","Unit D","San Fransico","CA","United States");
+('1','1','40000','123 Warf ave','Unit B','San Fransico','CA','United States');
+('2','2','36000','123 Warf ave','Unit B','San Fransico','CA','United States');
+('3','3','80000','77 Warf St','Unit D','San Fransico','CA','United States');
 
 SELECT * 
 FROM Employee.Department
 
 insert into Employee.Department(ID,[Name],[Location])
 Values
-(1,"Grill","Back of Bob's Burgers");
-(2,"Register","Front Of Bob's Burgers");
-(3,"Sales","DownTown Home Office");
+('1','Grill','Back of Bobs Burgers');
+('2','Register','Front Of Bobs Burgers');
+('3','Sales','DownTown Home Office');
+
+
+SELECT *
+FROM Employee.Employee
+insert into Employee.Employee(ID,FirstName,LastName,SSN,DeptID)
+Values
+('4','Tina','Smith','324-90-1234','4');
+
+SELECT * 
+FROM Employee.EmpDetails
+insert into Employee.EmpDetails
+Values Employee.EmpDetails(ID,EmployeeID,Salary,Address1,Address2,City,[State],Country)
+('4','4','45000','2 Wonder Warf ln','unit C','San Fransico','CA','United States');
+
+insert into Employee.Department(ID,[Name],[Location])
+Values
+('4','Marketing','Downtown Home Office');
+
+SELECT SUM(Salary)
+FROM Employee.EmpDetails as ED
+INNER Join Employee.Employee as EM on EM.ID = ED.ID
+LEFT Join Employee.Department as DP on DP.ID = EM.ID
+WHERE DP.Name is 'Marketing';
+
